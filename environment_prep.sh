4a47cbd5aaa9043f922ff8c21116a115eb351b0e
Linh B Ngo <lngo@clemson.edu>
2018-09-21 21:08:32 -0400

    Update environment_prep.sh

--- environment_prep.sh
+++ environment_prep.sh
@@ -6,3 +6,5 @@ sudo systemctl disable firewalld
 sudo service firewalld stop
 sudo setenforce 0
 sudo umask 0022
+
+sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
