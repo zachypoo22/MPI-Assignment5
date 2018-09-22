924b6f3c5533a0dacf119e89a0c985ee1ed2a3de
Linh B Ngo <lngo@clemson.edu>
2018-09-21 21:03:31 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -35,24 +35,17 @@ request.addTour(tour)
 
 link = request.LAN("lan")
 
-for i in range(6):
+for i in range(4):
   if i == 0:
-    node = request.XenVM("head")
-    node.routable_control_ip = "true"
-  elif i == 1:
-    node = request.XenVM("metadata")
-  elif i == 2:
-    node = request.XenVM("storage")
+    node = request.rawpc("namenode")
   else:
-    node = request.XenVM("compute-" + str(i))
-    node.cores = 2
-    node.ram = 4096
-    
+    node = request.XenVM("datanode-" + str(i))
+
+  node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
   
   iface = node.addInterface("if" + str(i-3))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
+  iface.component_id = "eth0"
   link.addInterface(iface)
   
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
