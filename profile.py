2f1c15d913442c5c0439c4987727237a00325f76
Linh B Ngo <lngo@clemson.edu>
2018-09-21 22:02:30 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -36,7 +36,8 @@ for i in range(4):
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
   
   iface = node.addInterface("if" + str(i-3))
-  iface.component_id = "eth0"
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
   
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
