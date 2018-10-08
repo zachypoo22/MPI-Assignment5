eb0eeacabfc0cb16fa3b67833b145507f92042fa
Zacheriah Mell <zachm93@live.com>
2018-10-08 17:45:41 -0400

    Update setup_storage.sh

--- setup_storage.sh
+++ setup_storage.sh
@@ -1,6 +1,7 @@
 ## Add /scratch
 sudo chkconfig --level 35 nfs on
 sudo mkdir scratch
+sudo chmod 777 /etc/exports
 while [ $i -le 3 ]
 do
 sudo echo "/scratch compute-$i(rw,sync,no_root_squash)" >> /etc/exports
