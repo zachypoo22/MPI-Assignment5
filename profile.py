bf8312420d08febab3805194e45b9d3716dd2ba5
Zacheriah Mell <zachm93@live.com>
2018-10-08 16:41:05 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -57,9 +57,8 @@ for i in range(6):
   
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
-  if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
   
   node.addService(pg.Execute(shell="sh", command="sudo su lngo -c 'cp /local/repository/source/* /users/lngo'"))
   
