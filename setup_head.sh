44ed46a37a9c2036233ffbb8ec07cf824c18ca12
Zacheriah Mell <zachm93@live.com>
2018-10-08 17:45:17 -0400

    Update setup_head.sh

--- setup_head.sh
+++ setup_head.sh
@@ -2,6 +2,7 @@
 sudo chkconfig --level 35 nfs on
 sudo mkdir software
 i = 1
+sudo chmod 777 /etc/exports
 while [ $i -le 3 ]
 do
 sudo echo "/software compute-$i(rw,sync,no_root_squash)" >> /etc/exports
