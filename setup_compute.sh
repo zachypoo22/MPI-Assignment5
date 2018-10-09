b2528ede8ebb03b29aa7460d83b9e465243d0bcc
Zacheriah Mell <zachm93@live.com>
2018-10-09 16:39:16 -0400

    Update setup_compute.sh

--- setup_compute.sh
+++ setup_compute.sh
@@ -10,5 +10,5 @@ sudo mkdir /mnt/scratch
 sudo mount -t nfs storage:/scratch /mnt/scratch
 
 ## Modify PATH
-sudo echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/zm875176/.bashrc
-sudo echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/zm875176/.bashrc
+sudo echo "export PATH='$PATH:/mnt/software/openmpi/3.1.2/bin'" >> /users/zm875176/.bashrc
+sudo echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/mnt/software/openmpi/3.1.2/lib/'" >> /users/zm875176/.bashrc
