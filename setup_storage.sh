c6b1a68567b445b365ad1e66f4ccb488e644c2ce
Zacheriah Mell <zachm93@live.com>
2018-10-09 13:57:16 -0400

    Update setup_storage.sh

--- setup_storage.sh
+++ setup_storage.sh
@@ -9,3 +9,7 @@ sudo echo "/scratch compute-$i(rw,sync,no_root_squash)" >> /etc/exports
 ((i++))
 done
 sudo service nfs start
+
+## Modify path
+sudo echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/zm875176/.bashrc
+sudo echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/zm875176/.bashrc
