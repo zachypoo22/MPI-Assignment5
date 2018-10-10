23ecdab623110f676538a8d33e347332a721001c
Zacheriah Mell <zachm93@live.com>
2018-10-10 15:29:39 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+sudo mkdir /test1
+
 set -x
 sudo yum -y group install "Development Tools"
+sudo mkdir /test2
+
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
 sudo ./configure --prefix=/software/openmpi/3.1.2
+sudo mkdir /test3
+
 sudo make
+sudo mkdir /test4
+
 sudo make all install
+sudo mkdir /test5
+
 cd ..
 sudo rm -Rf openmpi-3.1.2
 sudo rm -Rf openmpi-3.1.2.tar.gz
