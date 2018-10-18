a46eaf36b14eeb86ac5c2062284c379fb3b44b0f
Zacheriah Mell <zachm93@live.com>
2018-10-18 11:27:38 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -6,7 +6,7 @@ sudo touch /ISITDONE.log
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
-sudo ./configure --prefix=/mnt/software/openmpi/3.1.2
+sudo ./configure --prefix=/software/openmpi/3.1.2
 sudo echo "done configuring" >> /ISITDONE.log
 sudo make
 sudo echo "done making" >> /ISITDONE.log
@@ -15,7 +15,7 @@ sudo echo "done make all install" >> /ISITDONE.log
 cd ..
 sudo rm -Rf openmpi-3.1.2
 sudo rm -Rf openmpi-3.1.2.tar.gz
-sudo echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/zm875176/.bashrc
-sudo echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/zm875176/.bashrc
+sudo echo "export PATH='$PATH:/mnt/software/openmpi/3.1.2/bin'" >> /users/zm875176/.bashrc
+sudo echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/mnt/software/openmpi/3.1.2/lib/'" >> /users/zm875176/.bashrc
 source ~/.bashrc
 sudo echo "all done" >> /ISITDONE.log
