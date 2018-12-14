48e59e73115b41cbcf6d67ef28063c929fba596a
jtmuir <43188969+jtmuir@users.noreply.github.com>
2018-12-14 15:44:12 -0500

    Update s_compute.sh

--- s_compute.sh
+++ s_compute.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-systemctl enable slurmd.service
-systemctl start slurmd.service
+sudo yum install mariadb-server mariadb-devel -y
+
+export MUNGEUSER=991
+sudo groupadd -g $MUNGEUSER munge
+sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+export SLURMUSER=992
+sudo groupadd -g $SLURMUSER slurm
+sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+
+sudo yum install epel-release -y
+sudo yum install munge munge-libs munge-devel -y
+sudo yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
+sudo yum install ntp -y
