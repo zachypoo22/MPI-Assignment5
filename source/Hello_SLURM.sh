64bf0fdc5a3f9cdc1a83430bb2f9af29cb6bcdd3
abr1d <42777867+abr1d@users.noreply.github.com>
2018-12-14 21:39:28 -0500

    Update Hello_SLURM.sh

--- source/Hello_SLURM.sh
+++ source/Hello_SLURM.sh
@@ -3,15 +3,12 @@
 #SBATCH -N 12
 #SBATCH --ntasks-per-node 4
 
-#echo command to stdout
 set -x
 
-# where do I start?
 echo $SLURM_SUBMIT_DIR
 
 module load mpi/gcc_openmpi
 
-#Assuming hello.c is in $scratch
 cd /scratch
 mpicc hello.c
 
