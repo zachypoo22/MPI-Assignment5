be88d71e84f8babe127beeab93795ed0025c16df
abr1d <42777867+abr1d@users.noreply.github.com>
2018-12-14 20:47:23 -0500

    tested script

--- s_install.sh
+++ s_install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-yum install mariadb-server mariadb-devel -y
+sudo yum install mariadb-server mariadb-devel -y
 yum groupinstall 'Development Tools' -y
 
 export MUNGEUSER=981
-groupadd -g $MUNGEUSER munge
+sudo groupadd -g $MUNGEUSER munge
 useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
 export SlurmUSER=982
 groupadd -g $SlurmUSER slurm
@@ -12,42 +12,46 @@ useradd  -m -c "Slurm workload manager" -d /var/lib/slurm -u $SlurmUSER -g slurm
 
 
 # munge key goes here
+sudo yum install munge munge-libs munge-devel -y
 
+sudo /usr/sbin/create-munge-key
+sudo cp /etc/munge/munge.key /scratch
+sudo chown -R munge: /etc/munge/ var/log/munge/
+sudo chmod 0700 /etc/munge/ /var/log/munge/
 
+sudo systemctl enable munge
+sudo systemctl start munge
 
+sudo wget https://download.schedmd.com/slurm/slurm-18.08.3.tar.bz2
 
-
-systemctl enable munge
-systemctl start munge
-
-wget https://download.schedmd.com/slurm/slurm-18.08.3.tar.bz2
-
-tar xvjf slurm-18.08.3.tar.bz2
+sudo tar xvjf slurm-18.08.3.tar.bz2
 cd slurm-18.08.3
 
-./configure
-make
-make install
+#add status log
+sudo ./configure
+sudo make
+sudo make install
 
-cp /local/repository/slurm/slurm.conf /usr/local/etc/slurm.conf
+#copy slurm.conf PUT SLURM INTO SOURCE FOLDER
+sudo cp /local/repository/slurm/slurm.conf /usr/local/etc/slurm.conf
 
-mkdir /var/spool/slurmctld
-chown slurm: /var/spool/slurmctld
-chmod 755 /var/spool/slurmctld
-touch /var/log/slurmctld.log
-chown slurm: /var/log/slurmctld.log
-touch /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
-chown slurm: /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
-mkdir /var/spool/slurmd
-chown slurm: /var/spool/slurmd
-chmod 755 /var/spool/slurmd
-touch /var/log/slurmd.log
-chown slurm: /var/log/slurmd.log
+sudo mkdir /var/spool/slurmctld
+sudo chown slurm: /var/spool/slurmctld
+sudo chmod 755 /var/spool/slurmctld
+sudo touch /var/log/slurmctld.log
+sudo chown slurm: /var/log/slurmctld.log
+sudo touch /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
+sudo chown slurm: /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
+sudo mkdir /var/spool/slurmd
+sudo chown slurm: /var/spool/slurmd
+sudo chmod 755 /var/spool/slurmd
+sudo touch /var/log/slurmd.log
+sudo chown slurm: /var/log/slurmd.log
 
-systemctl stop firewalld
+sudo sudo systemctl stop firewalld
 systemctl disable firewalld
 
-yum install ntp -y
-chkconfig ntpd on
-ntpdate pool.ntp.org
-systemctl start ntpd
+sudo yum install ntp -y
+sudo chkconfig ntpd on
+sudo ntpdate pool.ntp.org
+sudo systemctl start ntpd
